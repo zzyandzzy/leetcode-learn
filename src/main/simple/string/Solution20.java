d4b0f1a9cdac83ffa3b11ae4d53f0ae708a6460e
intent <zzy.main@gmail.com>
2020-06-15 21:49:54 +0800

    Solution20valid-parentheses有效的括号.

---
+++ src/main/simple/string/Solution20.java
@@ -0,0 +1,98 @@
+package main.simple.string;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * main.simple.string
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * <p>
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "()"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: "()[]{}"
+ * 输出: true
+ * 示例 3:
+ * <p>
+ * 输入: "(]"
+ * 输出: false
+ * 示例 4:
+ * <p>
+ * 输入: "([)]"
+ * 输出: false
+ * 示例 5:
+ * <p>
+ * 输入: "{[]}"
+ * 输出: true
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author intent zzy.main@gmail.com
+ * @date 2020/6/15 9:03 下午
+ * @since 1.0
+ */
+public class Solution20 {
+    // Hash table that takes care of the mappings.
+    private static HashMap<Character, Character> mappings;
+
+    public void main(String[] args) {
+        mappings = new HashMap<Character, Character>();
+        mappings.put(')', '(');
+        mappings.put('}', '{');
+        mappings.put(']', '[');
+        System.out.println(isValid("()"));
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("(]"));
+        System.out.println(isValid("([)]"));
+        System.out.println(isValid("{[]}"));
+        System.out.println(isValid("{[}]"));
+        System.out.println(isValid("(([]){})"));
+        System.out.println(isValid(""));
+    }
+
+    /**
+     * 双指针
+     * 如果头部和尾部是一对就继续
+     * 或者头部和头部的下一个是一对就继续
+     *
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        // Initialize a stack to be used in the algorithm.
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // If the current character is a closing bracket.
+            if (mappings.containsKey(c)) {
+
+                // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                // If the mapping for this bracket doesn't match the stack's top element, return false.
+                if (topElement != mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                // If it was an opening bracket, push to the stack.
+                stack.push(c);
+            }
+        }
+
+        // If the stack still contains elements, then it is an invalid expression.
+        return stack.isEmpty();
+    }
+}
