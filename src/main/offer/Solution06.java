7ce51689fab2bad387b90972120b0929253e0bb5
intent <zzy.main@gmail.com>
2020-06-19 17:55:07 +0800

    剑指Offer Solution06.

---
+++ src/main/offer/Solution06.java
@@ -0,0 +1,74 @@
+package main.offer;
+
+import main.simple.list.ListNode;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * main.offer
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ *  
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= 链表长度 <= 10000
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author intent zzy.main@gmail.com
+ * @date 2020/6/19 5:45 下午
+ * @since 1.0
+ */
+public class Solution06 {
+    public static void main(String[] args) {
+        ListNode head1 = new ListNode(1);
+        head1.add(3, 2);
+        System.out.println(Arrays.toString(reversePrint(head1)));
+    }
+
+    /**
+     * 倒序输出
+     * 因为要返回数组，所以必须得到长度，要想得到长度只有遍历整个链表（除非数组扩容）
+     * 或者使用栈。、、
+     * <p>
+     * 执行用时：
+     * 2 ms
+     * , 在所有 Java 提交中击败了
+     * 49.88%
+     * 的用户
+     * 内存消耗：
+     * 40.6 MB
+     * , 在所有 Java 提交中击败了
+     * 100.00%
+     * 的用户
+     *
+     * @param head
+     * @return
+     */
+    public static int[] reversePrint(ListNode head) {
+        if (head == null) {
+            return new int[0];
+        }
+        Stack<Integer> stack = new Stack<>();
+        ListNode node = head;
+        while (node != null) {
+            stack.add(node.val);
+            node = node.next;
+        }
+        int[] arr = new int[stack.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = stack.pop();
+        }
+        return arr;
+    }
+}
