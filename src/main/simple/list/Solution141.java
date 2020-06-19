c0aaf0130dde5d6385e070f54e2ac88bb9f3c574
intent <zzy.main@gmail.com>
2020-06-19 10:57:39 +0800

    Solution141linked-list-cycle环形链表.

---
+++ src/main/simple/list/Solution141.java
@@ -0,0 +1,79 @@
+package main.simple.list;
+
+/**
+ * main.simple.list
+ * 给定一个链表，判断链表中是否有环。
+ * <p>
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 如果 pos 是 -1，则在该链表中没有环。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ * <p>
+ * <p>
+ *  
+ * <p>
+ * 进阶：
+ * <p>
+ * 你能用 O(1)（即，常量）内存解决此问题吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/linked-list-cycle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author intent zzy.main@gmail.com
+ * @date 2020/6/19 10:35 上午
+ * @since 1.0
+ */
+public class Solution141 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 快慢指针
+     * info
+     * 解答成功:
+     * 执行耗时:0 ms,击败了100.00% 的Java用户
+     * 内存消耗:39.7 MB,击败了5.49% 的Java用户
+     *
+     * @param head
+     * @return
+     */
+    public static boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        // 慢指针一次移动一个
+        ListNode slow = head;
+        // 快指针一次移动两个
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (slow == null || fast == null) {
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
