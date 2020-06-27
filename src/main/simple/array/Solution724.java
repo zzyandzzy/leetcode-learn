7efc9a83c8d305a017c139e9c98108d5f327ea33
intent <zzy.main@gmail.com>
2020-06-27 18:19:47 +0800

    Solution724find-pivot-index寻找数组的中心索引.

---
+++ src/main/simple/array/Solution724.java
@@ -0,0 +1,66 @@
+package main.simple.array;
+
+/**
+ * main.other
+ * 给定一个整数类型的数组 nums，请编写一个能够返回数组 “中心索引” 的方法。
+ * <p>
+ * 我们是这样定义数组 中心索引 的：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。
+ * <p>
+ * 如果数组不存在中心索引，那么我们应该返回 -1。如果数组有多个中心索引，那么我们应该返回最靠近左边的那一个。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：
+ * nums = [1, 7, 3, 6, 5, 6]
+ * 输出：3
+ * 解释：
+ * 索引 3 (nums[3] = 6) 的左侧数之和 (1 + 7 + 3 = 11)，与右侧数之和 (5 + 6 = 11) 相等。
+ * 同时, 3 也是第一个符合要求的中心索引。
+ * 示例 2：
+ * <p>
+ * 输入：
+ * nums = [1, 2, 3]
+ * 输出：-1
+ * 解释：
+ * 数组中不存在满足此条件的中心索引。
+ *  
+ * <p>
+ * 说明：
+ * <p>
+ * nums 的长度范围为 [0, 10000]。
+ * 任何一个 nums[i] 将会是一个范围在 [-1000, 1000]的整数。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-pivot-index
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author intent zzy.main@gmail.com
+ * @date 2020/6/27 5:55 下午
+ * @since 1.0
+ */
+public class Solution724 {
+    public static void main(String[] args) {
+        System.out.println(pivotIndex(new int[]{1, 7, 3, 6, 5, 6}));
+        System.out.println(pivotIndex(new int[]{}));
+        System.out.println(pivotIndex(new int[]{1}));
+        System.out.println(pivotIndex(new int[]{1, 2}));
+        System.out.println(pivotIndex(new int[]{1, 3, 1}));
+        System.out.println(pivotIndex(new int[]{-1, -1, -1, -1, -1, 0}));
+    }
+
+    public static int pivotIndex(int[] nums) {
+        int sum = 0, leftsum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftsum == sum - leftsum - nums[i]) {
+                return i;
+            }
+            leftsum += nums[i];
+        }
+        return -1;
+    }
+}
