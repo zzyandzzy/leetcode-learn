f02dc6a56d38165c4783db00188a55398bc4989f
intent <zzy.main@gmail.com>
2020-07-06 10:29:01 +0800

    Solution430flatten-a-multilevel-doubly-linked-list扁平化多级双向链表.

---
+++ src/main/medium/list/Solution430.java
@@ -0,0 +1,120 @@
+package main.medium.list;
+
+import main.util.Node;
+
+/**
+ * main.medium.list
+ * 多级双向链表中，除了指向下一个节点和前一个节点指针之外，它还有一个子链表指针，可能指向单独的双向链表。这些子列表也可能会有一个或多个自己的子项，依此类推，生成多级数据结构，如下面的示例所示。
+ * <p>
+ * 给你位于列表第一级的头节点，请你扁平化列表，使所有结点出现在单级双链表中。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+ * 输出：[1,2,3,7,8,11,12,9,10,4,5,6]
+ * 解释：
+ * <p>
+ * 输入的多级列表如下图所示：
+ * <p>
+ * <p>
+ * <p>
+ * 扁平化后的链表如下图：
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：head = [1,2,null,3]
+ * 输出：[1,3,2]
+ * 解释：
+ * <p>
+ * 输入的多级列表如下图所示：
+ * <p>
+ * 1---2---NULL
+ * |
+ * 3---NULL
+ * 示例 3：
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ *  
+ * <p>
+ * 如何表示测试用例中的多级链表？
+ * <p>
+ * 以 示例 1 为例：
+ * <p>
+ * 1---2---3---4---5---6--NULL
+ * |
+ * 7---8---9---10--NULL
+ * |
+ * 11--12--NULL
+ * 序列化其中的每一级之后：
+ * <p>
+ * [1,2,3,4,5,6,null]
+ * [7,8,9,10,null]
+ * [11,12,null]
+ * 为了将每一级都序列化到一起，我们需要每一级中添加值为 null 的元素，以表示没有节点连接到上一级的上级节点。
+ * <p>
+ * [1,2,3,4,5,6,null]
+ * [null,null,7,8,9,10,null]
+ * [null,11,12,null]
+ * 合并所有序列化结果，并去除末尾的 null 。
+ * <p>
+ * [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 节点数目不超过 1000
+ * 1 <= Node.val <= 10^5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/flatten-a-multilevel-doubly-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author intent zzy.main@gmail.com
+ * @date 2020/7/6 10:23 上午
+ * @since 1.0
+ */
+public class Solution430 {
+    public static void main(String[] args) {
+
+    }
+
+    private Node prev = null;
+
+    public Node flatten(Node head) {
+        dfs(head);
+        return head;
+    }
+
+    /**
+     * 执行用时：
+     * 0 ms
+     * , 在所有 Java 提交中击败了
+     * 100.00%
+     * 的用户
+     * 内存消耗：
+     * 37.7 MB
+     * , 在所有 Java 提交中击败了
+     * 50.00%
+     * 的用户
+     *
+     * @param head
+     */
+    private void dfs(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node next = head.next;
+        if (prev != null) {
+            prev.next = head;
+            head.prev = prev;
+        }
+        prev = head;
+        dfs(head.child);
+        head.child = null;
+        dfs(next);
+    }
+}
---
+++ src/main/util/Node.java
@@ -0,0 +1,15 @@
+package main.util;
+
+/**
+ * main.util
+ *
+ * @author intent zzy.main@gmail.com
+ * @date 2020/7/6 9:41 上午
+ * @since 1.0
+ */
+public class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+}
