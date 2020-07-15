5f8dd57da55c4c1dd136fcea317ad32b82196484
intent <zzy.main@gmail.com>
2020-07-15 22:08:12 +0800

    Solution剑指 Offer 20biao-shi-shu-zhi-de-zi-fu-chuan-lcof表示数值的字符串.

---
+++ leetcode/editor/cn/Solution剑指 Offer 20biao-shi-shu-zhi-de-zi-fu-chuan-lcof表示数值的字符串.java
@@ -0,0 +1,20 @@
+// id: 剑指 Offer 20
+// title: 表示数值的字符串
+// slug: biao-shi-shu-zhi-de-zi-fu-chuan-lcof
+//请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。例如，字符串"+100"、"5e2"、"-123"、"3.1416"、"0123"都表示数值，但
+//"12e"、"1a3.14"、"1.2.3"、"+-5"、"-1E-16"及"12e+5.4"都不是。 
+//
+// 
+//
+// 注意：本题与主站 65 题相同：https://leetcode-cn.com/problems/valid-number/ 
+// Related Topics 数学 
+// 👍 29 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isNumber(String s) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
