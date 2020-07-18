6f65836f44b215d6f853cf73b9a551a78af21103
intent <zzy.main@gmail.com>
2020-07-18 21:11:09 +0800

    Solution剑指 Offer 28dui-cheng-de-er-cha-shu-lcof对称的二叉树.

---
+++ src/main/offer/Solution28.java
@@ -0,0 +1,88 @@
+package main.offer;
+
+import main.util.TreeNode;
+
+/**
+ * main.offer
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ *  
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= 节点个数 <= 1000
+ * <p>
+ * 注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author intent zzy.main@gmail.com
+ * @date 2020/7/18 20:56
+ * @since 1.0
+ */
+public class Solution28 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 如果是数组就好办多了！！！
+     * 虽然可以循环遍历用数组来处理但是太废时间了。。。
+     * <p>
+     * 只有递归了
+     * <p>
+     * 执行用时：
+     * 0 ms
+     * , 在所有 Java 提交中击败了
+     * 100.00%
+     * 的用户
+     * 内存消耗：
+     * 38.2 MB
+     * , 在所有 Java 提交中击败了
+     * 100.00%
+     * 的用户
+     *
+     * @param root
+     * @return
+     */
+    public static boolean isSymmetric(TreeNode root) {
+        return root == null || isEqual(root.left, root.right);
+    }
+
+    public static boolean isEqual(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+        return isEqual(left.left, right.right) && isEqual(left.right, right.left);
+    }
+}
