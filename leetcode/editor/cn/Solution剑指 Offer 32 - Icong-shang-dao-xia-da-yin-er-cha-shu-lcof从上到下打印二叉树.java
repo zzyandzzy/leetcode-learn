a141b3e158d682712a84a0fa87550d7522d32739
intent <zzy.main@gmail.com>
2020-09-23 21:28:54 +0800

    Solution剑指 Offer 32 - Icong-shang-dao-xia-da-yin-er-cha-shu-lcof从上到下打印二叉树

---
+++ leetcode/editor/cn/Solution剑指 Offer 32 - Icong-shang-dao-xia-da-yin-er-cha-shu-lcof从上到下打印二叉树.java
@@ -0,0 +1,49 @@
+// id: 剑指 Offer 32 - I
+// title: 从上到下打印二叉树
+// slug: cong-shang-dao-xia-da-yin-er-cha-shu-lcof
+//从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。 
+//
+// 
+//
+// 例如: 
+//给定二叉树: [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回： 
+//
+// [3,9,20,15,7]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 节点总数 <= 1000 
+// 
+// Related Topics 树 广度优先搜索 
+// 👍 41 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] levelOrder(TreeNode root) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/main/offer/Solution32.java
@@ -0,0 +1,76 @@
+package main.offer;
+
+import main.util.TreeNode;
+import main.util.TreeOperation;
+
+import java.util.*;
+
+/**
+ * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+ * <p>
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回：
+ * <p>
+ * [3,9,20,15,7]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author intent zzy.main@gmail.com
+ * @date 2020/9/23 21:06
+ * @since 1.0
+ */
+public class Solution32 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        TreeNode right = new TreeNode(20);
+        right.left = new TreeNode(15);
+        right.right = new TreeNode(7);
+        root.right = right;
+        TreeOperation.show(root);
+        System.out.println(Arrays.toString(levelOrder(root)));
+    }
+
+    /**
+     * 用队列
+     *
+     * @param root 根
+     * @return
+     */
+    public static int[] levelOrder(TreeNode root) {
+        if (root == null) {
+            return new int[0];
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>() {
+            {
+                add(root);
+            }
+        };
+        List<Integer> ans = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            ans.add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        int[] res = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            res[i] = ans.get(i);
+        }
+        return res;
+    }
+}
