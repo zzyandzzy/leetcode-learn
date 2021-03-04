cc61f6eff0070af1b0d2be6761326e57fb86729a
intent <zzy.main@gmail.com>
2021-03-04 15:14:21 +0800

    重做剑指Offer！

--- src/main/offer/package-info.java
+++ src/main/offer/package-info.java
@@ -1,6 +1,7 @@
 /**
  * main.offer
  * 这个是《剑指Offer》的练习
+ * 第一次重做：2021年03月04日15:13:28
  * 题目：https://leetcode-cn.com/problemset/lcof/
  *
  * @author intent zzy.main@gmail.com
--- src/main/util/Node.java
+++ src/main/util/Node.java
@@ -1,5 +1,8 @@
 package main.util;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * main.util
  *
@@ -13,9 +16,53 @@ public class Node {
     public Node next;
     public Node child;
     public Node random;
-
+    public Node left;
+    public Node right;
 
     public Node(int val) {
         this.val = val;
+    }
+
+    // [5,4,8,11,null,13,4,7,2,null,null,5,1]
+    //
+    //              5
+    //             / \
+    //            4   8
+    //           /   / \
+    //          11  13  4
+    //         /  \    / \
+    //        7    2  5   1
+    public static Node createTree(Integer[] v) {
+        if (v.length == 0) {
+            return null;
+        }
+        //使用逐层遍历的方式，生成树
+        Queue<Node> q = new LinkedList<>();
+        Node root = new Node(v[0]);
+        q.add(root);
+        //用以表示当前数组可用的元素下标
+        int idx = 1;
+        while (!q.isEmpty() && idx < v.length) {
+            int cnt = q.size();
+            for (int i = 1; i <= cnt; i++) {
+                Node cur = q.poll();
+                //判断左子树是否为空
+                if (v[idx] != null) {
+                    cur.left = new Node(v[idx]);
+                    q.add(cur.left);
+                }
+                idx++;
+                if (idx >= v.length) {
+                    break;
+                }
+                //判断右子树是否为空
+                if (v[idx] != null) {
+                    cur.right = new Node(v[idx]);
+                    q.add(cur.right);
+                }
+                idx++;
+            }
+        }
+        return root;
     }
 }
